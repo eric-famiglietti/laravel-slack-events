0a61f20da7143d65098e92573c88a56362f90623
Eric Famiglietti <eric.famiglietti@gmail.com>
2018-03-11 21:08:02 -0400

    Add comment.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace LaravelSlackEvents\Tests;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+    // Dummy values from https://api.slack.com/events-api...
     protected $challenge = '3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P';
     protected $token = 'Jhj5dZrVaK7ZwHHjRyZWjbDl';
 
